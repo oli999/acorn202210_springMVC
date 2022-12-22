1223a1a2319edfb37155b52f84c4e85e1b61a1a2
kimgura <oli999@naver.com>
2022-12-22 16:02:35 +0900

    Spring04 에 검색기능 구현함

--- Spring04_Final/src/main/java/com/gura/spring04/file/service/FileServiceImpl.java
+++ Spring04_Final/src/main/java/com/gura/spring04/file/service/FileServiceImpl.java
@@ -1,6 +1,7 @@
 package com.gura.spring04.file.service;
 
 import java.io.File;
+import java.net.URLEncoder;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -42,12 +43,53 @@ public class FileServiceImpl implements FileService{
 		//보여줄 페이지의 끝 ROWNUM
 		int endRowNum=pageNum*PAGE_ROW_COUNT;
 		
+		/*
+			[ 검색 키워드에 관련된 처리 ]
+			-검색 키워드가 파라미터로 넘어올수도 있고 안넘어 올수도 있다.		
+		*/
+		String keyword=request.getParameter("keyword");
+		String condition=request.getParameter("condition");
+		//만일 키워드가 넘어오지 않는다면 
+		if(keyword==null){
+			//키워드와 검색 조건에 빈 문자열을 넣어준다. 
+			//클라이언트 웹브라우저에 출력할때 "null" 을 출력되지 않게 하기 위해서  
+			keyword="";
+			condition=""; 
+		}
+
+		//특수기호를 인코딩한 키워드를 미리 준비한다. 
+		String encodedK=URLEncoder.encode(keyword);
+			
+		//FileDto 객체에 startRowNum 과 endRowNum 을 담는다.
+		FileDto dto=new FileDto();
+		dto.setStartRowNum(startRowNum);
+		dto.setEndRowNum(endRowNum);
+	
+		//만일 검색 키워드가 넘어온다면 
+		if(!keyword.equals("")){
+			//검색 조건이 무엇이냐에 따라 분기 하기
+			if(condition.equals("title_filename")){//제목 + 파일명 검색인 경우
+				dto.setTitle(keyword);
+				dto.setOrgFileName(keyword);
+			}else if(condition.equals("title")){ //제목 검색인 경우
+				dto.setTitle(keyword);
+			}else if(condition.equals("writer")){ //작성자 검색인 경우
+				dto.setWriter(keyword);
+			} // 다른 검색 조건을 추가 하고 싶다면 아래에 else if() 를 계속 추가 하면 된다.
+		}
+		
+		
+		//파일 목록을 select 해 온다.(검색 키워드가 있는경우 키워드에 부합하는 전체 글) 
+		List<FileDto> list=dao.getList(dto);
+		
+		//전체 글의 갯수(검색 키워드가 있는경우 키워드에 부합하는 전체 글의 갯수)
+		int totalRow=dao.getCount(dto);
+		
 		//하단 시작 페이지 번호 
 		int startPageNum = 1 + ((pageNum-1)/PAGE_DISPLAY_COUNT)*PAGE_DISPLAY_COUNT;
 		//하단 끝 페이지 번호
 		int endPageNum=startPageNum+PAGE_DISPLAY_COUNT-1;
-		//전체 글의 갯수
-		int totalRow=dao.getCount( new FileDto() );
+		
 		//전체 페이지의 갯수 구하기
 		int totalPageCount=(int)Math.ceil(totalRow/(double)PAGE_ROW_COUNT);
 		//끝 페이지 번호가 이미 전체 페이지 갯수보다 크게 계산되었다면 잘못된 값이다.
@@ -55,19 +97,16 @@ public class FileServiceImpl implements FileService{
 			endPageNum=totalPageCount; //보정해 준다. 
 		}
 		
-		//FileDto 객체를 생성해서 
-		FileDto dto=new FileDto();
-		//위에서 계산된 startRowNum , endRowNum 을 담아서 
-		dto.setStartRowNum(startRowNum);
-		dto.setEndRowNum(endRowNum);
-		//파일 목록을 select 해 온다. 
-		List<FileDto> list=dao.getList(dto);
 		//응답에 필요한 데이터를 view page 에 전달하기 위해  request scope 에 담는다
 		request.setAttribute("list", list);
 		request.setAttribute("pageNum", pageNum);
 		request.setAttribute("startPageNum", startPageNum);
 		request.setAttribute("endPageNum", endPageNum);
 		request.setAttribute("totalPageCount", totalPageCount);
+		request.setAttribute("keyword", keyword);
+		request.setAttribute("encodedK", encodedK);
+		request.setAttribute("totalRow", totalRow); 
+		request.setAttribute("condition", condition);
 	}
 
 	@Override
--- Spring04_Final/src/main/webapp/WEB-INF/views/file/list.jsp
+++ Spring04_Final/src/main/webapp/WEB-INF/views/file/list.jsp
@@ -49,15 +49,16 @@
 			<ul class="pagination">
 				<%--
 					startPageNum 이 1 이 아닌 경우에만 Prev 링크를 제공한다. 
+					&condition=${condition}&keyword=${encodedK}
 				 --%>
 				<c:if test="${startPageNum ne 1 }">
 					<li class="page-item">
-						<a class="page-link" href="list?pageNum=${startPageNum-1 }">Prev</a>
+						<a class="page-link" href="list?pageNum=${startPageNum-1 }&condition=${condition}&keyword=${encodedK}">Prev</a>
 					</li>
 				</c:if>
 				<c:forEach var="i" begin="${startPageNum }" end="${endPageNum }">
 					<li class="page-item ${pageNum eq i ? 'active' : '' }">
-						<a class="page-link" href="list?pageNum=${i }">${i }</a>
+						<a class="page-link" href="list?pageNum=${i }&condition=${condition}&keyword=${encodedK}">${i }</a>
 					</li>
 				</c:forEach>
 				<%--
@@ -65,11 +66,28 @@
 				 --%>
 				<c:if test="${endPageNum lt totalPageCount }">
 					<li class="page-item">
-						<a class="page-link" href="list?pageNum=${endPageNum+1 }">Next</a>
+						<a class="page-link" href="list?pageNum=${endPageNum+1 }&condition=${condition}&keyword=${encodedK}">Next</a>
 					</li>
 				</c:if>
 			</ul>
 		</nav>
+		<!-- 검색 폼 -->
+		<form action="list" method="get">
+			<label for="condition">검색조건</label>	
+			<select name="condition" id="condition">
+				<option value="title_filename" ${condition eq 'title_filename' ? 'selected' : '' }>제목 + 파일명</option>
+				<option value="title" ${condition eq 'title' ? 'selected' : '' }>제목</option>
+				<option value="writer" ${condition eq 'writer' ? 'selected' : '' }>작성자</option>
+			</select>
+			<input type="text" name="keyword" placeholder="검색어..." value="${keyword }"/>
+			<button type="submit">검색</button>
+		</form>
+		<c:if test="${not empty condition }">
+			<p>
+				<strong>${totalRow }</strong> 개의 자료가 검색 되었습니다.
+				<a href="list">리셋</a>
+			</p>
+		</c:if>
 	</div>
 	<script>
 		function deleteConfirm(num){
@@ -81,3 +99,7 @@
 	</script>
 </body>
 </html>
+
+
+
+
